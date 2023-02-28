eb921462f6778a2121b826d64ca72919f6fcd62c
Michael Doswell <mike.doswell@gmail.com>
2023-02-28 08:17:58 -0800

    Added logo and icon files, updated index.html to use new logo and colours

--- scripts/firebaseAPI_BBY12.js
+++ scripts/firebaseAPI_BBY12.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: "AIzaSyB2l7Y5LHDgAw-4xP2xggGBTn2R8NcINgg",
-    authDomain: "project-4730739388541914931.firebaseapp.com",
-    projectId: "project-4730739388541914931",
-    storageBucket: "project-4730739388541914931.appspot.com",
-    messagingSenderId: "459916249899",
-    appId: "1:459916249899:web:5cf53be77da61c3f48936e"
+    apiKey: "AIzaSyChNk8KYH_mYdonhnu3m9kjTACcjwVPvFU",
+    authDomain: "comp1800-bby12.firebaseapp.com",
+    projectId: "comp1800-bby12",
+    storageBucket: "comp1800-bby12.appspot.com",
+    messagingSenderId: "133189907233",
+    appId: "1:133189907233:web:ac3e8d9067b9649cc0c460"
 };
 
 //--------------------------------------------
