b37b80cf59531503fcf3360ed0a623e6ff595693
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-03-01 08:46:18 -0800

    nav and footer

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,10 @@ function sayHello() {
 function loginPage(){
     window.location.href = 'login.html';
 }
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+      $('.animated-icon1').toggleClass('open');
+    });
+  });
