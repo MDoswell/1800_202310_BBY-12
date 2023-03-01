4857ea1d5b298e48f178601b647ead1b1bd28e26
Michael Doswell <mike.doswell@gmail.com>
2023-03-01 09:30:01 -0800

    adding modal demo

--- scripts/loginSkeleton.js
+++ scripts/loginSkeleton.js
@@ -5,6 +5,6 @@
 function loadSkeleton(){
     console.log($('#loginNavbarPlaceholder').load('./text/loginNav.html'));
     console.log($('#loginFooterPlaceholder').load('./text/loginFooter.html'));
-    //console.log($('#modalPlaceholder').load('./text/hazard.html'));    
+    console.log($('#modalPlaceholder').load('./text/hazard.html'));    
 }
 loadSkeleton();  //invoke the function
