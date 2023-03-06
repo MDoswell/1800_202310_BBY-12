845d73c8bb904cd7a1addc71b4fcb6b44d6324f8
Michael Doswell <mike.doswell@gmail.com>
2023-03-05 16:42:36 -0800

    combined skeleton.js and loginSkeleton.js, and moved sample hazard button from profile.html to main.html

--- scripts/profile.js
+++
@@ -1,6 +0,0 @@
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
-    console.log($('#modalPlaceholder').load('./text/hazard.html'));    
-}
-loadSkeleton();  //invoke the function
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
+}
+
 function sayHello() {
     
 }
 //sayHello();
+
+
 
 function loginPage(){
     window.location.href = 'login.html';
