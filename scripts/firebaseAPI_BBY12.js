67117c101498357983e53f6a82657fa0bebeb87b
Michael Doswell <mike.doswell@gmail.com>
2023-03-22 09:19:35 -0700

    fixed image storage bug

--- scripts/firebaseAPI_BBY12.js
+++ scripts/firebaseAPI_BBY12.js
@@ -18,4 +18,4 @@ var firebaseConfig = {
 //--------------------------------------------
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-//const storage = firebase.storage();
+const storage = firebase.storage();
